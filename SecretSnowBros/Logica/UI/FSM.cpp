ece95244fbe6ae47a47c23a56fe864c1ed540a28
Francisco Martin Tolaba <ftolaba@itba.edu.ar>
2018-12-02 22:19:14 -0300

    nada importante

--- SecretSnowBros/Logica/UI/FSM.cpp
+++ SecretSnowBros/Logica/UI/FSM.cpp
@@ -5,30 +5,29 @@ void FSM::setUp(AllegroWindow& window, AllegroSoundFactory& soundF) {
 }
 
 int FSM::runTitleMenu(void* data){
+	titleMenu->run;
 	return 0;
-
 }
 
 int FSM::runInstructionMenu(void * data) {
+	instructionMenu->run;
 	return 0;
-
 }
 
 int FSM::runGameOverMenu(void * data) {
+	gameOverMenu->run;
 	return 0;
 }
 
 int FSM::runWonMenu(void * data) {
+	wonMenu->run;
 	return 0;
-
 }
 
 int FSM::runGame(void*data) {
 	return 0;
-
 }
 
 int FSM::runExit(void*data) {
 	return 0;
-
 }
