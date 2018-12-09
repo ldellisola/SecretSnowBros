d57a5008a2acb91bd907faf23ea60fd1cd9d4d17
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2018-12-09 20:35:37 -0300

    nada

--- SecretSnowBros/MVC/Controllers/AllegroController.cpp
+++ SecretSnowBros/MVC/Controllers/AllegroController.cpp
@@ -1,25 +1,12 @@
 #include "AllegroController.h"
 
 
-
-Local2PlayerController::Local2PlayerController(ALLEGRO_EVENT_QUEUE * eventQueue, uint16_t player1ID, uint16_t player2ID)
-	:eventFactory(eventQueue),Player1ID(player1ID),Player2ID(player2ID){}
-
-void * Local2PlayerController::getEvent(void * data)
-{
-	runEv();
-
-	void * temp = nullptr;
-	if (events.size() != 0) {
-		temp = events.front();	
-		events.pop();
-	}
-
-	return temp;
-}
+#include "allegro5/allegro.h"
 
 Local1PlayerController::Local1PlayerController(ALLEGRO_EVENT_QUEUE * eventQueue, uint16_t player1ID)
-	:eventFactory(eventQueue), Player1ID(player1ID) {}
+	:eventFactory(eventQueue), Player1ID(player1ID) {
+
+}
 
 void * Local1PlayerController::getEvent(void * data)
 {
@@ -76,34 +63,3 @@ void Local1PlayerController::runEv()
 }
 
 
-
-void Local2PlayerController::runEv()
-{
-
-	if (eventFactory.getEvent()) {
-		uint16_t id;
-		GameEventType type = GameEventType::None;
-		switch (eventFactory.getEventType())
-		{
-		case ALLEGRO_EVENT_KEY_DOWN:			
-			switch (eventFactory.getKeyboardEvent().keycode)
-			{
-			case ALLEGRO_KEY_W:	type = GameEventType::Jump; id = Player1ID; break;
-			case ALLEGRO_KEY_A:	type = GameEventType::Left; id = Player1ID; break; 
-			case ALLEGRO_KEY_D:	type = GameEventType::Right; id = Player1ID; break;
-			case ALLEGRO_KEY_C:	type = GameEventType::Shoot; id = Player1ID; break;
-			case ALLEGRO_KEY_UP:	type = GameEventType::Jump; id = Player2ID; break;
-			case ALLEGRO_KEY_LEFT:	type = GameEventType::Left; id = Player2ID; break;
-			case ALLEGRO_KEY_RIGHT:	type = GameEventType::Right; id = Player2ID; break;
-			case ALLEGRO_KEY_M:		type = GameEventType::Shoot; id = Player2ID; break;
-			}
-			break;
-		case ALLEGRO_EVENT_TIMER:
-			type = GameEventType::Timer;
-			break;
-		}
-
-		if (type != GameEventType::None) 
-			this->events.push(new GameEvent(type, id,true));
-	}
-}
--- SecretSnowBros/MVC/Controllers/AllegroController.h
+++ SecretSnowBros/MVC/Controllers/AllegroController.h
@@ -24,19 +24,3 @@ private:
 	const uint16_t Player1ID;
 };
 
-
-
-class Local2PlayerController : public Controller
-{
-public:
-	Local2PlayerController(ALLEGRO_EVENT_QUEUE * eventQueue, uint16_t player1ID, uint16_t player2ID = NAN);
-
-	void * getEvent(void * data);
-private:
-	AllegroEventFactory eventFactory;
-
-	void runEv();
-
-	std::queue<GameEvent*> events;
-	const uint16_t Player1ID, Player2ID;
-};
